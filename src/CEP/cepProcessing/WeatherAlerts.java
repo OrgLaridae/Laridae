ad520b2e5a4b37eef382febfbc9e1bc7fc39a151
ruvenij <ruvenij@gmail.com>
2016-01-21 02:03:05 +0530

    cleaned

--- src/CEP/cepProcessing/WeatherAlerts.java
+++ src/CEP/cepProcessing/WeatherAlerts.java
@@ -7,7 +7,6 @@ import org.wso2.siddhi.core.SiddhiManager;
 import org.wso2.siddhi.core.event.Event;
 import org.wso2.siddhi.core.query.output.callback.QueryCallback;
 import org.wso2.siddhi.core.stream.input.InputHandler;
-import org.wso2.siddhi.core.util.EventPrinter;
 
 
 /**
@@ -30,7 +29,6 @@ import org.wso2.siddhi.core.util.EventPrinter;
 public class WeatherAlerts {
     private SiddhiManager siddhiManager;
     private InputHandler madisInputHandler;
-    private InputHandler radarInputHandler;
 
     Main listener;
 
