a11f0ea4d137c879683c7b79d704284e98165a21
ruvenij <ruvenij@gmail.com>
2016-01-20 23:47:13 +0530

    changed distance calculation

--- src/CEP/sidhdhiExtention/IsNearStation.java
+++ src/CEP/sidhdhiExtention/IsNearStation.java
@@ -16,10 +16,9 @@ import org.wso2.siddhi.query.api.extension.annotation.SiddhiExtension;
 @SiddhiExtension(namespace = "madis", function = "isNearStation")
 public class IsNearStation extends FunctionExecutor {
     Attribute.Type returnType;
-    private static final double DISTANCE_THRESHOLD = 35;
-    private static final double EARTH_RADIUS = 6371;//in kilometers
+    private static final double DISTANCE_THRESHOLD = 40;
     private double latA = 0, lonA = 0, latB = 0, lonB = 0, dLat = 0, dLng = 0, a = 0, c = 0, dist = 0;
-
+    boolean isNear=false;
     @Override
     public void init(Attribute.Type[] types, SiddhiContext siddhiContext) {
         returnType = Attribute.Type.BOOL;
@@ -34,24 +33,33 @@ public class IsNearStation extends FunctionExecutor {
 
     @Override
     protected Object process(Object o) {
-        boolean isNear = false;
+        isNear = false;
         if ((o instanceof Object[]) && ((Object[]) o).length == 4) {
             latA = Double.parseDouble(String.valueOf(((Object[]) o)[0]));
             lonA = Double.parseDouble(String.valueOf(((Object[]) o)[1]));
             latB = Double.parseDouble(String.valueOf(((Object[]) o)[2]));
             lonB = Double.parseDouble(String.valueOf(((Object[]) o)[3]));
-            dLat = Math.toRadians(latB - latA);
-            dLng = Math.toRadians(lonB - lonA);
+//            dLat = Math.toRadians(latB - latA);
+//            dLng = Math.toRadians(lonB - lonA);
+//
+//            //calculates the actual distance
+//            a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latA)) * Math.cos(Math.toRadians(latB)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+//            c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//            dist = (double) (EARTH_RADIUS * c);
 
-            //calculates the actual distance
-            a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latA)) * Math.cos(Math.toRadians(latB)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
-            c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            dist = (double) (EARTH_RADIUS * c);
+            if(latA==latB && lonA==lonB){
+                isNear=false;
+            }else{
+                double theta = lonB - lonA;
+                double dist = Math.sin(deg2rad(latB)) * Math.sin(deg2rad(latA)) + Math.cos(deg2rad(latB)) * Math.cos(deg2rad(latA)) * Math.cos(deg2rad(theta));
+                dist = Math.acos(dist);
+                dist = rad2deg(dist);
+                dist = dist * 60 * 1.1515*1.609344;
 
-            if (dist < DISTANCE_THRESHOLD) {
-                isNear = true;
+                if (dist < DISTANCE_THRESHOLD) {
+                    isNear = true;
+                }
             }
-
         }
         return isNear;
     }
@@ -59,6 +67,14 @@ public class IsNearStation extends FunctionExecutor {
     @Override
     public void destroy() {
 
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
     @Override
