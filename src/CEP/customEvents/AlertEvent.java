25bef0abb8298196b78114442c2ff2f8c6dfaa31
ruvenij <ruvenij@gmail.com>
2016-01-24 02:56:22 +0530

    changed input parameter

--- src/CEP/customEvents/AlertEvent.java
+++ src/CEP/customEvents/AlertEvent.java
@@ -1,15 +1,16 @@
 package CEP.customEvents;
 
+import java.util.ArrayList;
 import java.util.EventObject;
 
 public class AlertEvent  extends EventObject{
-    private String location;
-    public AlertEvent(Object source, String coordinates){
+    private ArrayList<Location> location;
+    public AlertEvent(Object source, ArrayList<Location> coordinates){
         super(source);
         location=coordinates;
     }
 
-    public String getCoordinates(){
+    public ArrayList<Location> getCoordinates(){
         return location;
     }
 }
