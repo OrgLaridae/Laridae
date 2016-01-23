0ed093f880cfe10c86280422ff148e067f970c68
ruvenij <ruvenij@gmail.com>
2016-01-24 02:57:16 +0530

    changed handleAlertEvent to detect the coordinates array list

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,7 +1,9 @@
 package GUI;
 
+import CEP.cepProcessing.CEPEnvironment;
 import CEP.customEvents.AlertEvent;
 import CEP.customEvents.BoundaryEvent;
+import CEP.customEvents.Location;
 import CEP.main.CEP;
 import ML.KMeansClass;
 import gov.nasa.worldwind.*;
@@ -21,7 +23,12 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 
@@ -30,8 +37,7 @@ import org.apache.spark.mllib.linalg.Vectors;
  *
  * @version $Id: SimplestPossibleExample.java 1171 2013-02-11 21:45:02Z dcollins $
  */
-public class Main extends JFrame
-{
+public class Main extends JFrame {
     JButton DataLoadButton;
     JButton CEPButton;
     JButton MLButton;
@@ -51,37 +57,35 @@ public class Main extends JFrame
     ArrayList<Vector> dataPoints;
     //ShapeAttributes normalAttributes;
 
-    public Main()
-    {
+    public Main() {
 
         initUI();
     }
 
     private static final String APP_NAME = "Laridae";
 
-    static
-    {
-        if (Configuration.isMacOS())
-        {
+    static {
+        if (Configuration.isMacOS()) {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             System.setProperty("com.apple.mrj.application.apple.menu.about.name", APP_NAME);
             System.setProperty("com.apple.mrj.application.growbox.intrudes", "false");
         }
     }
-    private void initUI(){
+
+    private void initUI() {
 
 
-        JPanel basic  = new JPanel();
+        JPanel basic = new JPanel();
         basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
         add(basic);
 
-        basic.add(Box.createRigidArea(new Dimension(0,20)));
+        basic.add(Box.createRigidArea(new Dimension(0, 20)));
 
         JPanel buttons = initButtons();
 
 
         basic.add(buttons);
-        basic.add(Box.createRigidArea(new Dimension(0,10)));
+        basic.add(Box.createRigidArea(new Dimension(0, 10)));
 
         JPanel map = new JPanel();
 
@@ -121,73 +125,91 @@ public class Main extends JFrame
         setLocationRelativeTo(null);
     }
 
-    public static void insertBeforeCompass(WorldWindow wwd, Layer layer)
-    {
+    public static void insertBeforeCompass(WorldWindow wwd, Layer layer) {
         // Insert the layer into the layer list just before the compass.
         int compassPosition = 0;
         LayerList layers = wwd.getModel().getLayers();
-        for (Layer l : layers)
-        {
+        for (Layer l : layers) {
             if (l instanceof CompassLayer)
                 compassPosition = layers.indexOf(l);
         }
         layers.add(compassPosition, layer);
     }
 
-    private void loadData(){
+    private void loadData() {
         //createPoint();
     }
 
-    private void runCEP(){
+    private void runCEP() {
 
         CEP.run(this);
     }
 
-    private void runML(){
-        ArrayList<Vector>[] clusters= KMeansClass.run(dataPoints, 20, 1);
+    private void runML() {
+        ArrayList<Vector>[] clusters = KMeansClass.run(dataPoints, 20, 1);
 
         for (int i = 0; i < clusters.length; i++) {
             Vector[] bounds = KMeansClass.getBounds(clusters[i], 360, 180);
-            double lat1 = bounds[0].apply(0)-90;
-            double lon1 = bounds[0].apply(1)-180;
-            double lat2 = bounds[1].apply(0)-90;
-            double lon2 = bounds[1].apply(1)-180;
+            double lat1 = bounds[0].apply(0) - 90;
+            double lon1 = bounds[0].apply(1) - 180;
+            double lat2 = bounds[1].apply(0) - 90;
+            double lon2 = bounds[1].apply(1) - 180;
 
             ShapeAttributes attributes = configureBounderyAttributes(Material.ORANGE, Material.ORANGE);
 
             LatLon upperLeft = LatLon.fromDegrees(lat1, lon1);
             LatLon lowerRight = LatLon.fromDegrees(lat2, lon2);
+            System.out.println("Lon : "+lon1+" "+lon2);
+            System.out.println("Lat : "+lat1+" "+lat2);
             createBoundery(MLBoundLayer, attributes, upperLeft, lowerRight, "ML boundary");
         }
     }
 
-    private void runWRF(){
+    private void runWRF() {
 
     }
 
-    public void handleAlertEvent(AlertEvent e){
-        String string = e.getCoordinates();
-        String[] coordinates = string.split(",");
-
+    public void handleAlertEvent(AlertEvent e) {
+        ArrayList<Location> array=e.getCoordinates();
         dataPoints = new ArrayList<>();
+        ShapeAttributes attributes;
 
-        for(String co:coordinates){
-            String[] latlon = co.split(":");
-            double lat = Double.parseDouble(latlon[0]);
-            double lon = Double.parseDouble(latlon[1]);
+        for (Location co : array) {
+            double lat = Double.parseDouble(co.getLatitude());
+            double lon = Double.parseDouble(co.getLongitude());
 
-            ShapeAttributes attributes = configurePointAttributes(Material.RED);
+            attributes = configurePointAttributes(Material.RED);
             createPoint(dataPointLayer, attributes, LatLon.fromDegrees(lat, lon));
 
-            double latPositive = lat+90;
-            double lonPositive = lon+180;
+            double latPositive = lat + 90;
+            double lonPositive = lon + 180;
+
             dataPoints.add(Vectors.dense(lonPositive, latPositive));
         }
+//        System.out.println(dataPoints.size());
+
+//        String string = e.getCoordinates();
+//        String[] coordinates = string.split(",");
+//
+//        dataPoints = new ArrayList<>();
+//
+//        for(String co:coordinates){
+//            String[] latlon = co.split(":");
+//            double lat = Double.parseDouble(latlon[0]);
+//            double lon = Double.parseDouble(latlon[1]);
+//
+//            ShapeAttributes attributes = configurePointAttributes(Material.RED);
+//            createPoint(dataPointLayer, attributes, LatLon.fromDegrees(lat, lon));
+//
+//            double latPositive = lat+90;
+//            double lonPositive = lon+180;
+//            dataPoints.add(Vectors.dense(lonPositive, latPositive));
+//        }
 
         CEPButton.setEnabled(false);
     }
 
-    public void handelBoundaryEvent(BoundaryEvent e){
+    public void handelBoundaryEvent(BoundaryEvent e) {
         String boundary = e.getBoundary();
         String[] values = boundary.split(" ");
         double minLat = Double.parseDouble(values[0]);
@@ -206,7 +228,7 @@ public class Main extends JFrame
     }
 
     private ShapeAttributes configureShapeAttributes
-            (Material outerColor, Material innerColor, double innerOpacity, double outerOpacity){
+            (Material outerColor, Material innerColor, double innerOpacity, double outerOpacity) {
 
         ShapeAttributes attributes = new BasicShapeAttributes();
         attributes.setInteriorMaterial(innerColor);
@@ -220,16 +242,16 @@ public class Main extends JFrame
 
         return attributes;
     }
-    
-    private ShapeAttributes configureBounderyAttributes(Material outerColor, Material innerColor){
+
+    private ShapeAttributes configureBounderyAttributes(Material outerColor, Material innerColor) {
         return configureShapeAttributes(outerColor, innerColor, 0.5, 0.7);
     }
-    
-    private ShapeAttributes configurePointAttributes(Material color){
+
+    private ShapeAttributes configurePointAttributes(Material color) {
         return configureShapeAttributes(color, color, 1, 1);
     }
 
-    private void createBoundery(RenderableLayer layer, ShapeAttributes attributes, LatLon upperLeft, LatLon lowerRight, String displayName){
+    private void createBoundery(RenderableLayer layer, ShapeAttributes attributes, LatLon upperLeft, LatLon lowerRight, String displayName) {
 
         double lat1 = upperLeft.getLatitude().getDegrees();
         double lon1 = upperLeft.getLongitude().getDegrees();
@@ -250,7 +272,7 @@ public class Main extends JFrame
         worldWindow.redraw();
     }
 
-    private void createPoint(RenderableLayer layer, ShapeAttributes attributes, LatLon pointLatLon){
+    private void createPoint(RenderableLayer layer, ShapeAttributes attributes, LatLon pointLatLon) {
 
         SurfaceCircle point = new SurfaceCircle(pointLatLon, 10000);
 
@@ -259,7 +281,7 @@ public class Main extends JFrame
         worldWindow.redraw();
     }
 
-    private JPanel initButtons(){
+    private JPanel initButtons() {
         JPanel buttons = new JPanel();
 
         DataLoadButton = new JButton("Load Weather Data");
@@ -312,8 +334,7 @@ public class Main extends JFrame
         return buttons;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
             JFrame frame = new Main();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
