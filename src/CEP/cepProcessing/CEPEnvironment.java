f02464cefd13350954514346efc0bab9d7d01033
ruvenij <ruvenij@gmail.com>
2016-01-24 20:09:00 +0530

    automated wrf moved to another package

--- src/CEP/cepProcessing/CEPEnvironment.java
+++ src/CEP/cepProcessing/CEPEnvironment.java
@@ -8,9 +8,9 @@ public class CEPEnvironment {
     public static final int THRESHOLD_LIFTED_INDEX=-1;
     public static final int THRESHOLD_HELICITY=150;
     public static final int THRESHOLD_INHIBITION=-50;
-    public static final String LIFTED_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Best(4layer)liftedindex.csv";
-    public static final String HELICITY_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/stormrelativehelicity.csv";
-    public static final String CONVECTIVE_INHIBITION_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/convectiveInhibition.csv";
+    public static final String LIFTED_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Lifted/best4layerliftedindexfile7.csv";
+    public static final String HELICITY_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Helicity/stormrelativehelicity7.csv";
+    public static final String CONVECTIVE_INHIBITION_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Convective/convective7.csv";
     public static final double DISTANCE_THRESHOLD = 40;
     public static final String COORDINATE_FILE_PATH="/home/ruveni/Data/Test.txt";
 }
