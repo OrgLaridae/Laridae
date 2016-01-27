dd154b4ed2a96fee8b3f3eee7465c33daa00de38
ruvenij <ruvenij@gmail.com>
2016-01-27 09:02:54 +0530

    cep window time reduced

--- src/CEP/cepProcessing/CEPEnvironment.java
+++ src/CEP/cepProcessing/CEPEnvironment.java
@@ -4,7 +4,7 @@ package CEP.cepProcessing;
  * Created by ruveni on 1/21/16.
  */
 public class CEPEnvironment {
-    public static final int TIME_GAP = 1; //in minutes
+    public static final int TIME_GAP = 8; //in seconds
     public static final int THRESHOLD_LIFTED_INDEX=-1;
     public static final int THRESHOLD_HELICITY=150;
     public static final int THRESHOLD_INHIBITION=-50;
@@ -12,5 +12,4 @@ public class CEPEnvironment {
     public static final String HELICITY_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Helicity/stormrelativehelicity7.csv";
     public static final String CONVECTIVE_INHIBITION_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Convective/convective7.csv";
     public static final double DISTANCE_THRESHOLD = 40;
-    public static final String COORDINATE_FILE_PATH="/home/ruveni/Data/Test.txt";
 }
