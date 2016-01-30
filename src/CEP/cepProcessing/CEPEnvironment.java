aa9013139403a206a186d0326894c0f396948464
ruvenij <ruvenij@gmail.com>
2016-01-30 23:22:28 +0530

    relative paths added

--- src/CEP/cepProcessing/CEPEnvironment.java
+++ src/CEP/cepProcessing/CEPEnvironment.java
@@ -8,8 +8,8 @@ public class CEPEnvironment {
     public static final int THRESHOLD_LIFTED_INDEX=-1;
     public static final int THRESHOLD_HELICITY=150;
     public static final int THRESHOLD_INHIBITION=-50;
-    public static final String LIFTED_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Lifted/best4layerliftedindexfile7.csv";
-    public static final String HELICITY_INDEX_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Helicity/stormrelativehelicity7.csv";
-    public static final String CONVECTIVE_INHIBITION_FILE_PATH="/home/ruveni/IdeaProjects/DataAgent/src/main/java/org/mora/cep/csvFiles/Convective/convective7.csv";
+    public static final String LIFTED_INDEX_FILE_PATH="src/CEP/csvFiles/Lifted/best4layerliftedindexfile7.csv";
+    public static final String HELICITY_INDEX_FILE_PATH="src/CEP/csvFiles/Helicity/stormrelativehelicity7.csv";
+    public static final String CONVECTIVE_INHIBITION_FILE_PATH="src/CEP/csvFiles/Convective/convective7.csv";
     public static final double DISTANCE_THRESHOLD = 40;
 }
