e69552d5e591b58d9cf1fd0870c6377e7b05bd97
viduranga <vidura.me@gmail.com>
2016-02-01 17:59:50 +0530

    Lambert <-> Lat lon

---
+++ src/Util/Lambert_LatLon.java
@@ -0,0 +1,18 @@
+package Util;
+
+public class Lambert_LatLon {
+    public static double[] latLonToLambert(double centralMeridian, double latitude, double longitude){
+        double x = longitude - centralMeridian;
+        double y = Math.sin(latitude);
+        return new double[]{x, y};
+    }
+
+    public static double[] lambertToLatLon(double centralMeridian, double x, double y){
+        double longitude = x +centralMeridian;
+        double latitude = Math.asin(y);
+
+        return new double[]{latitude, longitude};
+    }
+
+
+}
