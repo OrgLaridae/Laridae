6b3137c5d397c29ace479ddf8b3c73add1b4264b
viduranga <vidura.me@gmail.com>
2016-02-01 19:03:54 +0530

    LatLon -> Lambert Cone

--- src/Util/Lambert_LatLon.java
+++ src/Util/Lambert_LatLon.java
@@ -1,17 +1,33 @@
 package Util;
 
 public class Lambert_LatLon {
-    public static double[] latLonToLambert(double centralMeridian, double latitude, double longitude){
-        double x = longitude - centralMeridian;
-        double y = Math.sin(latitude);
+    public static double[] latLonToLambert
+            (double latitude, double refLatitude, double standardParallel1,  double standardParallel2,
+             double longitude, double refLongitude){
+        double F = F(standardParallel1, standardParallel2);
+        double n = n(standardParallel1, standardParallel2);
+        double p = p(latitude, n, F);
+        double p0 = p0(refLatitude, n, F);
+
+        double x = p * Math.sin(n*(longitude-refLongitude));
+        double y = p0 - p*Math.cos(n*(longitude-refLongitude));
         return new double[]{x, y};
     }
 
-    public static double[] lambertToLatLon(double centralMeridian, double x, double y){
-        double longitude = x +centralMeridian;
-        double latitude = Math.asin(y);
 
-        return new double[]{latitude, longitude};
+    public static double n(double st1, double st2){
+        return Math.log10(Math.cos(st1)/Math.cos(st2)) / Math.log10(Math.tan((Math.PI/4)+st2/2)/Math.tan((Math.PI/4)+st1/2));
     }
 
+    public static double p(double latitude, double n, double F){
+        return F/Math.pow(Math.tan(Math.PI/4 + latitude/2),n);
+    }
+
+    public static double p0(double refLatitude, double n, double F){
+        return p(refLatitude, n, F);
+    }
+
+    public static double F(double st1, double n){
+        return Math.cos(st1) * Math.pow(Math.tan(Math.PI/4 + st1/2),n) / n;
+    }
 }
