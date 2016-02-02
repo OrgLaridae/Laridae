c5af96773c24260248a48eb486d56b34a4d0629d
viduranga <vidura.me@gmail.com>
2016-02-02 13:56:10 +0530

    Lambert to lat lon conversion added

--- src/Util/Lambert_LatLon.java
+++ src/Util/Lambert_LatLon.java
@@ -2,8 +2,8 @@ package Util;
 
 public class Lambert_LatLon {
     public static double[] latLonToLambert
-            (double latitude, double refLatitude, double standardParallel1,  double standardParallel2,
-             double longitude, double refLongitude){
+            (double latitude, double longitude, double refLatitude, double standardParallel1,  double standardParallel2,
+             double refLongitude){
         double F = F(standardParallel1, standardParallel2);
         double n = n(standardParallel1, standardParallel2);
         double p = p(latitude, n, F);
@@ -15,19 +15,47 @@ public class Lambert_LatLon {
     }
 
 
-    public static double n(double st1, double st2){
+    private static double n(double st1, double st2){
         return Math.log10(Math.cos(st1)/Math.cos(st2)) / Math.log10(Math.tan((Math.PI/4)+st2/2)/Math.tan((Math.PI/4)+st1/2));
     }
 
-    public static double p(double latitude, double n, double F){
+    private static double p(double latitude, double n, double F){
         return F/Math.pow(Math.tan(Math.PI/4 + latitude/2),n);
     }
 
-    public static double p0(double refLatitude, double n, double F){
+    private static double p0(double refLatitude, double n, double F){
         return p(refLatitude, n, F);
     }
 
-    public static double F(double st1, double n){
+    private static double F(double st1, double n){
         return Math.cos(st1) * Math.pow(Math.tan(Math.PI/4 + st1/2),n) / n;
+    }
+
+    public static double[] lambertToLatLon
+            (double x, double y, double refLatitude, double standardParallel1,  double standardParallel2,
+              double refLongitude){
+        double F = F(standardParallel1, standardParallel2);
+        double n = n(standardParallel1, standardParallel2);
+        double p0 = p0(refLatitude, n, F);
+
+        double q = q(x, y, n, p0);
+        double theta = theta(x, y, p0);
+
+        double latitude = 2*Math.atan(Math.pow(F/q, 1/n)) - Math.PI/2;
+
+        double longitude = refLongitude + theta/n;
+
+        return new double[]{latitude, longitude};
+    }
+
+    private static double q(double x, double y, double n, double p0){
+        return sign(n) * Math.sqrt(Math.pow(x, 2) + Math.pow(p0-y, 2));
+    }
+
+    private static double theta(double x, double y, double p0){
+        return Math.atan(x/(p0-y));
+    }
+    private static int sign(double val){
+        return val>0? 1:-1;
     }
 }
